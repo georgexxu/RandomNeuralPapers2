c861362a456b8d1d7faaec40fd384aab8f30530f
linye-boli <linye@changchunboli.com>
2025-08-27 21:59:13 +0800

    clean l2fit code

--- code/rfm_l2fitting_ly/utils.py
+++ code/rfm_l2fitting_ly/utils.py
@@ -1,8 +1,21 @@
 import torch 
+import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+import numpy as np 
+import itertools
+import scipy 
+import math
+import matplotlib.pyplot as plt
+torch.set_default_dtype(torch.float64)
+pi = torch.tensor(np.pi,dtype=torch.float64)
 
-def PiecewiseGQ2D_weights_points(Nx, order,bl = [-1,-1],ur = [1,1], device=None): 
+def SquareMesh2D_points(xmin=-1, xmax=1, Nx=150):
+    x = torch.linspace(xmin, xmax,Nx)
+    X1, X2 = torch.meshgrid(x,x, indexing='ij')
+    x_test = torch.concat([X1.reshape(-1,1), X2.reshape(-1,1)], dim=1)
+    return x_test
+
+def PiecewiseGQ2D_weights_points(Nx, order,bl = [-1,-1],ur = [1,1]): 
     """ A slight modification of PiecewiseGQ2D function that only needs the weights and integration points.
     Parameters
     Allows a symmetric square domain (around 0) with lower left corner at bl and upper right corner at ur 
@@ -16,7 +29,6 @@ def PiecewiseGQ2D_weights_points(Nx, order,bl = [-1,-1],ur = [1,1], device=None)
     long_weights: torch.tensor
     integration_points: torch.tensor
     """
-#     print("order: ",order )
     x, w = np.polynomial.legendre.leggauss(order)
     gauss_pts = np.array(np.meshgrid(x,x,indexing='ij')).reshape(2,-1).T
     weights =  (w*w[:,None]).ravel()
@@ -37,31 +49,78 @@ def PiecewiseGQ2D_weights_points(Nx, order,bl = [-1,-1],ur = [1,1], device=None)
     ordered_pairs = np.array(np.meshgrid(index,index,indexing='ij'))
     ordered_pairs = ordered_pairs.reshape(2,-1).T
 
-    # print(ordered_pairs)
-    # print()
     ordered_pairs = torch.tensor(ordered_pairs)
-    # print(ordered_pairs.size())
     ordered_pairs = torch.tile(ordered_pairs, (1,order**2)) # number of GQ points
-    # print(ordered_pairs)
 
     ordered_pairs =  ordered_pairs.reshape(-1,2)
-    # print(ordered_pairs)
     translation = ordered_pairs*h + (torch.tensor(bl) + h/2) 
-    # print(translation)
 
     integration_points = integration_points + translation 
-#     print(integration_points.size())
-    # func_values = integrand2_torch(integration_points)
-    return long_weights.to(device), integration_points.to(device)
+    return long_weights, integration_points
 
-def assemble_least_square_linear_system_quadrature(model,rhs, integration_weights,integration_points): 
-    """ Assemble the least square linear system, D: design matrix, b right hand side """
-    in_feat, out_feat = model.fc1.weight.shape 
-    relu_k = model.k 
-    W = model.fc1.weight.data  # shape: (M, d+1)
-    b = model.fc1.bias.data  # shape: (M,)
-    Z = integration_points@ W.t() + b  
-    D = (F.relu(Z)**relu_k)  * integration_weights  
-    b_rhs = rhs(integration_points) * integration_weights 
-    return D,b_rhs
+def s2_uniform_grid(neuron_nums):
+    indices = torch.arange(0, neuron_nums, dtype=torch.float64) + 0.5
+    phi = torch.acos(1 - 2*indices/neuron_nums)
+    theta = pi * (1 + 5**0.5) * indices
+    x = torch.sin(phi) * torch.cos(theta)
+    y = torch.sin(phi) * torch.sin(theta)
+    z = torch.cos(phi)
+    points = torch.stack((x, y, z), dim=1)
+    return points 
 
+def s2_uniform_grid_init(neuron_num, dims=2, k=1):
+    points = s2_uniform_grid(neuron_num)
+    def create_mesh_grid(dims, pts):
+        mesh = torch.tensor(list(itertools.product(pts,repeat=dims)))
+        vertices = mesh.reshape(len(pts) ** dims, -1) 
+        return vertices
+    
+    counter = 0 
+    pts = torch.tensor([0.,1.])  
+    # pts = torch.tensor([-1.,1.])
+    positions = create_mesh_grid(dims,pts) 
+    neuron_num = points.shape[0]
+    relu_k = k
+    ws = []
+    bs = []
+    poly_dofs = math.comb(relu_k + dims, dims)
+    for i in range(neuron_num): 
+        w = points[i:i+1,0:2]
+        b = points[i,2]
+        values = torch.matmul(positions,w.T)
+        left_end = - torch.max(values)
+        right_end = - torch.min(values)
+        offset = (right_end - left_end)/50
+        if b > left_end + offset/2 and b < right_end - offset/2: 
+            ws.append(w)
+            bs.append(b)
+        elif b >= right_end - offset/2 and counter < poly_dofs:
+            ws.append(w)
+            bs.append(b)
+            counter += 1
+    
+    ws = torch.concatenate(ws)
+    bs = torch.tensor(bs)
+    return ws, bs
+
+# show convergence order 
+def output_convergence_order_l2(neuron_nums,err_list_l2): 
+    print("$n$ & \t $\|u-u_n \|_{L^2}$ & \t order  \\\ \hline \hline ")
+    for i, item in enumerate(err_list_l2):
+        if i == 0: 
+            print("{} \t\t & {:.3e} &\t\t *  \\\ \hline  \n".format(neuron_nums[i],item))    
+        else: 
+            print("{} \t\t &  {:.3e} &  \t\t {:.2f}  \\\ \hline  \n".format(neuron_nums[i],item, np.log(err_list_l2[i-1]/err_list_l2[i])/np.log(neuron_nums[i]/neuron_nums[i-1]) ) )
+
+def plot_err_convergence(relu_k,d,actual_neuron_arr,mean_err_l2_arr,title): 
+    optimal_rate = 1/2 + (2 * relu_k +1)/(2*d)
+    ref = mean_err_l2_arr[0] *actual_neuron_arr[0]**optimal_rate * (actual_neuron_arr)**(-optimal_rate)
+    plt.plot(actual_neuron_arr,ref,'-.',label = 'slope =  -{}'.format(optimal_rate))
+    plt.plot(actual_neuron_arr, mean_err_l2_arr, '.-', label = 'ReLU$^{}$'.format(relu_k), linewidth = 2)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.legend()
+    plt.ylabel("rel $L^2$ error")
+    plt.xlabel("Number of neurons n")
+    plt.title(title)
+    plt.show() 
